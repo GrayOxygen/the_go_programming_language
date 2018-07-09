fae3ae6145156be9aa20073ef7b77dbc817ee66b
辉氧 <490750217@qq.com>
2018-07-10 00:25:27 +0800

    Update chapter1.go

--- chapters/chapter1.go
+++ chapters/chapter1.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"log"
 )
 
 //模拟unix的echo指令
@@ -128,46 +129,90 @@ func DupFromFile2() {
 var palette = []color.Color{color.White, color.Black}
 
 const (
-	whiteIndex = 0 //画板中的第一种颜色
-	blackIndex = 1 //画板中的下一种颜色
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
 )
 
 //GIF动画
 func Paint() {
+
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
 	rand.Seed(time.Now().UTC().UnixNano())
+
 	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
 		handler := func(w http.ResponseWriter, r *http.Request) {
 			lissajous(w)
 		}
 		http.HandleFunc("/", handler)
-		http.ListenAndServe("localhost:8000", nil)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
 		return
 	}
+
 	lissajous(os.Stdout)
 }
 
 func lissajous(out io.Writer) {
 	const (
-		cycles  = 5
-		res     = 0.001
-		size    = 100
-		nframes = 64
-		delay   = 8
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
 	)
-	freq := rand.Float64() * 3.0
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0
+	phase := 0.0 // phase difference
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
-	gif.EncodeAll(out, &anim)
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+//获取请求响应的数据，模拟linux工具curl
+func Fetch() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch:%v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
+func Fetch2() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch:%v\n", err)
+			os.Exit(1)
+		}
+		var b io.Writer
+		bts, err := io.Copy(b, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", bts)
+	}
 }
